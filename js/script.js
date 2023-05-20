4d40e8e0f5a1832a449da4658e43e2be9c3e2f29
KeriBoss <levanlam3447@gmail.com>
2023-05-20 11:42:09 +0700

    create booking, select date and form successful page html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+mobiscroll.datepicker('#myInput', {
+    controls: ['calendar'],
+    selectMultiple: false,
+    dateFormat: 'DD/MM/YYYY',
+    display: 'anchored',
+    touchUi: true,
+    themeVariant: 'light'
+});

b74489c693482094c0d218ac86779fec949f001f
KeriBoss <levanlam3447@gmail.com>
2023-05-22 15:54:53 +0700

    update form add service

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+
+
+
+//
+const schedule = document.querySelector('.schedule-main');
+const cardStaff = document.querySelectorAll('.card-staff');
+const booking = document.querySelector('.booking-main');
+console.log('dasd');
+if(cardStaff, schedule){
+    cardStaff.forEach(data => {
+        var cardItem = data.querySelectorAll('.card-item a');
+        cardItem.forEach(item => {
+            item.addEventListener('click', function(event){
+                event.preventDefault();
+                booking.classList.add('unshow')
+                schedule.classList.add('active')
+            })
+        })
+    })
+}
 mobiscroll.datepicker('#myInput', {
     controls: ['calendar'],
     selectMultiple: false,
